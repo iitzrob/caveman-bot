b3685f355967a29d31533ec96eb99f7da82e342c
iitzrob <rizzbut3@gmail.com>
2026-09-22 13:39:30 +1000

    Update config.js

--- cavemanbot/cavemanbot/config.js
+++ cavemanbot/cavemanbot/config.js
@@ -257,7 +257,7 @@ module.exports = {
   // There's also a Mark as Paid button on every tracker for the times the
   // balances can't prove it (e.g. the payer was earning money at the same time).
   payments: {
-    statsChannelId: '',
+    statsChannelId: '1551189729909809154',
     statsBotId: '1321520416677695559',
     statsCommand: '!stats',
     replyTimeoutSeconds: 20,
